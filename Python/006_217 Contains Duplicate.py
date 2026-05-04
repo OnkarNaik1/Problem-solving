40b49e7547ac64de2b1a717cad6ceba2e40fb482
Onkar Naik <onkar.sampat.naik.1999@gmail.com>
2026-05-05 02:44:53 +0530

    Added code for Day 6

---
+++ Python/006_217 Contains Duplicate.py
@@ -0,0 +1,8 @@
+#217. Contains Duplicate
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        st = set(nums)
+        if len(st) == len(nums):
+            return False
+        else:
+            return True
